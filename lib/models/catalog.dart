2e1d9b509e486119797b8282fd3c535fafe6e069
Aman <60567275+aman007-cpu@users.noreply.github.com>
2022-06-17 00:03:40 +0530

    completedday6

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,10 +1,23 @@
+class CatalogModel {
+  static List<Item> items = [
+    Item(
+        id: 1,
+        name: "iphone 12 pro",
+        desc: "Apple iphone 12th generator",
+        price: 992,
+        color: "#33505a",
+        image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2QcKLfKsTkrkRGdUKv2kaGUm_VvNv7oJQhQ&usqp=CAU")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
-  final String img;
+  final String image;
 
   Item(
       {required this.id,
@@ -12,16 +25,23 @@ class Item {
       required this.desc,
       required this.price,
       required this.color,
-      required this.img});
-}
+      required this.image});
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+        id: map["id"],
+        name: map["name"],
+        desc: map["desc"],
+        price: map["price"],
+        color: map["color"],
+        image: map["image"]);
+  }
 
-final products = [
-  Item(
-      id: "Code11",
-      name: "iphone 12 pro",
-      desc: "Apple iphone 12th generator",
-      price: 992,
-      color: "#33505a",
-      img:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2QcKLfKsTkrkRGdUKv2kaGUm_VvNv7oJQhQ&usqp=CAU")
-];
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "img": image
+      };
+}
--- lib/pages/Home_page.dart
+++ lib/pages/Home_page.dart
@@ -1,18 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drwable.dart';
+import 'package:flutter_application_1/widgets/item_widget.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final int days = 30;
+
   final String name = "aman kaintura";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("AK APP"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of flutter $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return Itemwidget(
+              item: CatalogModel.items[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
